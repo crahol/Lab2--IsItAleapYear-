7ba24c87c8b035338a36f1ccefa6a4117ba7bf30
crahol <raholchaitali20@gmail.com>
2021-06-06 22:33:17 -0400

    Lab-2 IsItALeapYearSubmitted

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        public bool IsItALeapYear(int yr)
+        {
+            if ((yr % 4 == 0) && (yr % 100 != 0 || yr % 400 == 0))
+            {
+                Console.WriteLine(yr + "is a leap year");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(yr + "is not a leap year");
+                return false;
+            }
+        }
+        static void Main(string[] args)
+       // static bool Program (yr)
+        {
+            Console.WriteLine("Is it a leap year?!");
+        }
+    }
+}
